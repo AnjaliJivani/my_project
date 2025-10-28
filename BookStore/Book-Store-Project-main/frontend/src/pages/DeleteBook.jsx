fd92238775833d55f2d181ed1ac6f89ec2ef5986
anjali <anjalijivani70@gmail.com>
2025-10-28 18:40:50 +0530

    bookstore project

--- BookStore/Book-Store-Project-main/frontend/src/pages/DeleteBook.jsx
+++ BookStore/Book-Store-Project-main/frontend/src/pages/DeleteBook.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
